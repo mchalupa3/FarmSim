ab169f29b3571d6f5aaad8193256be4dfa8ad015
Michael Chalupa <mchalupa3@gmail.com>
2019-04-18 10:40:35 -0500

    update

--- src/Plots.java
+++ src/Plots.java
@@ -11,4 +11,25 @@ public void setTime(double t){
     cropTime =t;
 }
 
+public double getCropTime(){
+    return cropTime;
+}
+
+public boolean isPlotFilled(){
+    return plotFilled;
+}
+
+public void displayPlot(){
+    System.out.println( "_________________________" );
+    System.out.println("|_ _ _ _ _ _ _ _ _ _ _ _ _|" );
+    System.out.println("|_ _ _ _ _ _ _ _ _ _ _ _ _|" );
+    System.out.println("|_ _ _ _ _ _ _ _ _ _ _ _ _|" );
+    System.out.println("|_ _ _ _ _ _ _ _ _ _ _ _ _|" );
+    System.out.println("|_ _ _ _ _ _ _ _ _ _ _ _ _|" );
+    System.out.println("|_ _ _ _ _ _ _ _ _ _ _ _ _|" );
+    System.out.println("|_ _ _ _ _ _ _ _ _ _ _ _ _|" );
+    System.out.println("|_ _ _ _ _ _ _ _ _ _ _ _ _|" );
+    System.out.println("|_________________________|" );
+}
+
 }
