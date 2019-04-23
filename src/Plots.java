9bae6f98aec7ce1ba59f111261fe2b3b809bed72
Michael Chalupa <mchalupa3@gmail.com>
2019-04-23 10:16:32 -0500

    update

--- src/Plots.java
+++ src/Plots.java
@@ -7,6 +7,19 @@ public void setPlotStatus(boolean  b){
     plotFilled = b;
 }
 
+public static void main(String[] args){
+    int[][] plots = new int[3][3];
+    for(int i = 0; i < 4; i++ ){
+        for(int b = 0; b < 4; b++){
+
+        }
+    }
+}
+
+
+
+
+
 public void setTime(double t){
     cropTime =t;
 }
