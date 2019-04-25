3b90e7980538f1e9fa535b1e4e542e19be5a099e
Michael Chalupa <mchalupa3@gmail.com>
2019-04-25 10:43:37 -0500

    update

--- src/Inventory.java
+++ src/Inventory.java
@@ -2,6 +2,7 @@ public class Inventory {
     public static int wheat = 0;
     public static int corn = 0;
     public static int soybeans = 0;
+    public static int money = 0;
 
 
 }
--- src/Selling.java
+++ src/Selling.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Selling {
     public static void main(String[] args){
 
@@ -6,6 +8,25 @@ public class Selling {
 
     public static void  market(){
         System.out.println( "Welcome to the market, what would you like to sell?" );
+        Scanner input = new Scanner( System.in );
+        String userInput = input.nextLine();
+        if(userInput.equals( "Corn" )){
+            System.out.println( "You have " + Inventory.corn + " bushels of corn. " );
+            System.out.println( "How much would you like to sell?" );
+            Inventory.money = input.nextInt() * 25;
+            System.out.println( "Your balance is now " + Inventory.money );
+        }
+        if(userInput.equals( "Soybeans" )){
+            System.out.println( "You have " + Inventory.corn + " bushels of soybeans. " );
+            System.out.println( "How much would you like to sell?" );
+            Inventory.money = input.nextInt() * 35;
+            System.out.println( "Your balance is now " + Inventory.money );
+        }if(userInput.equals( "Wheat" )){
+            System.out.println( "You have " + Inventory.corn + " bushels of wheat. " );
+            System.out.println( "How much would you like to sell?" );
+            Inventory.money = input.nextInt() * 15;
+            System.out.println( "Your balance is now " + Inventory.money );
+        }
 
     }
 
